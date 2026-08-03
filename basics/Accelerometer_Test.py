81de7e1e3ca1fbd05e4548882be6bc533902ac3d
AahaanVatsa <aahaan.vatsa@gmail.com>
2026-08-03 16:03:57 -0500

    Update Accelerometer_Test.py

--- basics/Accelerometer_Test.py
+++ basics/Accelerometer_Test.py
@@ -7,15 +7,15 @@ from machine import Pin, I2C
 i2c = I2C(0, sda = Pin(4), scl = Pin(5), freq = 400000)
 imu = MPU6050(i2c, device_addr = 0x68)
 
-#Infinite loop
+#Infinite loop to constantly read values
 while True:
 
-  #Takes readings for linear acceleration in three dimesion
+  #Take readings for linear acceleration in three dimesions
   ax = round(imu.accel.x, 2) #Acceleration forward and back
   ay = round(imu.accel.y, 2) #Acceleration left and right
   az = round(imu.accel.z, 2) #Acceleration up and down
 
-  #Prints results on serial monitor
+  #Print results on serial monitor
   print(" ")
   print("ax = ", ax, " m/s^2", "ay = ", ay, " m/s^2", "az = ", az, " m/s^2")
   sleep(0.1)
