62aba042eaa98663dff7e497d17ae8b3c56eb1d2
AahaanVatsa <aahaan.vatsa@gmail.com>
2026-08-03 16:04:34 -0500

    Update Gyroscope_Test.py

--- basics/Gyroscope_Test.py
+++ basics/Gyroscope_Test.py
@@ -7,15 +7,15 @@ from machine import Pin, I2C
 i2c = I2C(0, sda = Pin(4), scl = Pin(5), freq = 400000)
 imu = MPU6050(i2c, device_addr = 0x68)
 
-#Iinfinite loop
+#Infinite loop to constantly read values
 while True:
 
-  #Takes reading for rotation in three dimesions (Roll, Pitch, Yaw)
+  #Take readings for rotation in three dimesions (Roll, Pitch, Yaw)
   gx = round(imu.gyro.x, 2) #Rotation around front-back axis
   gy = round(imu.gyro.y, 2) #Rotation around lef-right axis
   gz = round(imu.gyro.z, 2) #Rotation around up-down axis
   
-  #Prints results in serial monitor
+  #Print results in serial monitor
   print(" ")
   print("gx = ", gx, " m/s^2", "gy = ", gy, " m/s^2", "gz = ", gz, " m/s^2")
   sleep(0.01)
