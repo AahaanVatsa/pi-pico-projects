6e911def9e29db9fc4b3a7006750f1f3879c0196
AahaanVatsa <aahaan.vatsa@gmail.com>
2026-08-03 16:12:37 -0500

    Update RF_Remote.py

--- basics/RF_Remote.py
+++ basics/RF_Remote.py
@@ -1,16 +1,19 @@
+# Import necessary modules and functions
 from machine import Pin
 from time import sleep
 
+# Initialize RF remote buttons
 rf_A = Pin(13, Pin.IN)
 rf_B = Pin(20, Pin.IN)
 rf_C = Pin(19, Pin.IN)
 rf_D = Pin(18, Pin.IN)
 
+# Continuously check and display button values
 while True:
-  A = rf_A.value()
-  B = rf_B.value()
-  C = rf_C.value()
-  D = rf_D.value()
+A = rf_A.value()
+B = rf_B.value()
+C = rf_C.value()
+D = rf_D.value()
 
-  print("A: ", A, " B: ", B, " C: ", C, " D: ", D)
-  sleep(0.5)
+print("A: ", A, " B: ", B, " C: ", C, " D: ", D)
+sleep(0.5)
