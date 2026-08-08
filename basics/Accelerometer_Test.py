072e3dedce8019cec2548223ac48ecdc383805de
AahaanVatsa <aahaan.vatsa@gmail.com>
2026-08-08 17:26:09 -0500

    Update Accelerometer_Test.py

--- basics/Accelerometer_Test.py
+++ basics/Accelerometer_Test.py
@@ -10,12 +10,12 @@ imu = MPU6050(i2c, device_addr = 0x68)
 # Constantly check linear acc. and display values
 while True:
 
-  #Take readings for linear acceleration in three dimesions
-  ax = round(imu.accel.x, 2) #Acceleration forward and back
-  ay = round(imu.accel.y, 2) #Acceleration left and right
-  az = round(imu.accel.z, 2) #Acceleration up and down
+  # Take readings for linear acceleration in three dimesions
+  ax = round(imu.accel.x, 2) # Acceleration forward and back
+  ay = round(imu.accel.y, 2) # Acceleration left and right
+  az = round(imu.accel.z, 2) # Acceleration up and down
 
-  #Print results on serial monitor
+  # Print results on serial monitor
   print(" ")
   print("ax = ", ax, " m/s^2", "ay = ", ay, " m/s^2", "az = ", az, " m/s^2")
   sleep(0.1)
