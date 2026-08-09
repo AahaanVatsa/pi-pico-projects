60380d2707c5d7658dda687d7d798f16278bf015
AahaanVatsa <aahaan.vatsa@gmail.com>
2026-08-09 11:30:37 -0500

    Update Wifi_Bot.py

--- communication/Wifi_Bot.py
+++ communication/Wifi_Bot.py
@@ -1,36 +1,42 @@
+# Import necessary modules and functions
 from time import sleep
 import network
 import motion
 from wifi_connection import RobotServer
 
+# Define WiFi credentials
 WIFI_NAME = "URWIFINAME"
 WIFI_PASSWORD = "URWIFIPWD"
 
+# Intialize and stop motors
 motion.init()
 motion.stop()
 
-wlan = network.WLAN(network.STA_IF)
+# Initialize and connect to WiFi
+wlan = network.WLAN(network.STA_IF) # Create station interface(WiFi client)
 wlan.active(True)
 wlan.connect(WIFI_NAME, WIFI_PASSWORD)
 
+# Keep trying until connected
 print("Connecting...")
 while not wlan.isconnected():
     sleep(0.5)
 
-print("Robot IP:", wlan.ifconfig()[0])
+print("Robot IP:", wlan.ifconfig()[0]) # Print robot IP address
 
-robot = RobotServer()
+robot = RobotServer() # Create an object 'robot' of class RobotServer
 
+# Main loop
 while True:
-    command = robot.get_command()
+    command = robot.get_command() # Receive command from web dashboard
 
-    if command == "F":
+    if command == "F": # Move forward
         motion.forward()
-    elif command == "B":
+    elif command == "B": # Move backward
         motion.backward()
-    elif command == "L":
+    elif command == "L": # Move left
         motion.left()
-    elif command == "R":
+    elif command == "R": # Move right
         motion.right()
-    else:
+    else: # Stop robot
         motion.stop()
