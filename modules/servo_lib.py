e0169cd4b8b81373bec92736c85146ab936b8a1f
AahaanVatsa <aahaan.vatsa@gmail.com>
2026-08-09 12:06:16 -0500

    Update servo_lib.py

--- modules/servo_lib.py
+++ modules/servo_lib.py
@@ -1,31 +1,42 @@
 
+# Import necessary modules and functions
 from machine import Pin, PWM
 from time import sleep
 
-class Servo:  
-    # Constants for the PWM signal
+# Define Servo class
+class Servo:
+
+    # Define constants for the PWM signal
     PWM_FREQ = 50  # 50Hz for most standard servos
-    
-    # Duty cycle values for 0 and 180 degrees
+
+    # Define duty cycle values for 0 and 180 degrees
     MIN_DUTY = 2000
     MAX_DUTY = 8000
 
+    # Define function to initialize servo
     def __init__(self, pin_num):
         self.pwm = PWM(Pin(pin_num))
         self.pwm.freq(self.PWM_FREQ)
 
+    # Define function to set the servo angle
     def set_angle(self, angle):
-        # Clamp the angle to the valid range
+
+        # Keep the angle within the valid range
         if angle < 0:
             angle = 0
         elif angle > 180:
             angle = 180
-            
-        # Map the angle to the duty cycle range
-        duty = int(self.MIN_DUTY + (angle / 180) * (self.MAX_DUTY - self.MIN_DUTY))
-        
+
+        # Convert the angle to a PWM duty cycle
+        duty = int(
+            self.MIN_DUTY +
+            (angle / 180) *
+            (self.MAX_DUTY - self.MIN_DUTY)
+        )
+
         # Set the PWM duty cycle
         self.pwm.duty_u16(duty)
 
+    # Define function to turn off servo PWM
     def deinit(self):
         self.pwm.deinit()
