ad4e97e3922e55bc82dbdf3fda72ce132036677c
AahaanVatsa <aahaan.vatsa@gmail.com>
2026-08-09 11:10:59 -0500

    Update RF_Servo_Control.py

--- communication/RF_Servo_Control.py
+++ communication/RF_Servo_Control.py
@@ -1,32 +1,41 @@
+# Import necessary modules and functions
 from machine import Pin
 from time import sleep
 import ultrasonic
 from servo_lib import Servo
 
+# Define servo motor pin
 servo_motor = Servo(11)
 
+# Initialize ultrasonic sensor
 ultrasonic.init()
 
+# Define RF remote button pins
 rf_A = Pin(13, Pin.IN)
 rf_B = Pin(20, Pin.IN)
 
+# Define motor speed
 speed = 32768
 
+# Define starting angle
 angle = 90
+
 servo_motor.set_angle(angle)
 
-sleep(0.75)
+sleep(0.75) # Small delay to complete initialization
 
+# Main loop
 while True:
+  # Read values for buttons
   A = rf_A.value()
   B = rf_B.value()
-  distance = ultrasonic.get_distance()
-  servo_motor.set_angle(angle)
+  distance = ultrasonic.get_distance() # Measure distance
+  servo_motor.set_angle(angle) # Set servo to current angle value
   print("Distance: ", distance, " cm", " | Servo Angle: ", angle)
-  if A == 1:
+  if A == 1: # Turn towards 0 degrees
     if angle > 0:
       angle -= 10
-  if B == 1:
+  if B == 1: # Turn towards 180 degress
     if angle < 180:
       angle += 10
   sleep(0.5)
