d89ee83f408105a727ae59fb32e3e235adc270cb
AahaanVatsa <aahaan.vatsa@gmail.com>
2026-08-09 12:21:05 -0500

    Update wifi_connection.py

--- modules/wifi_connection.py
+++ modules/wifi_connection.py
@@ -1,14 +1,23 @@
 
+# Import necessary modules and functions
 import socket
 
+# Define RobotServer class
 class RobotServer:
+
+    # Define function to initialize server
     def __init__(self):
         self.live_data = "T:--,H:--,A:--,SAFE:0,MSG:Status: Waiting for data"
+
+        # Create and configure the server socket
         self.server = socket.socket()
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # Bind the server to port 80
         self.server.bind(("0.0.0.0", 80))
         self.server.listen(1)
 
+    # Define function to update live sensor data
     def send_data(self, temperature, humidity, air_value, safe_status=0, safety_message="Status: Normal"):
         self.live_data = "T:{},H:{},A:{},SAFE:{},MSG:{}".format(
             temperature,
@@ -18,22 +27,27 @@ class RobotServer:
             safety_message
         )
 
+    # Define function to receive and process commands
     def get_command(self):
         client, address = self.server.accept()
         request = client.recv(1024).decode()
 
+        # Respond to OPTIONS requests
         if request.startswith("OPTIONS"):
             self.reply(client, "ok")
             return None
 
+        # Respond to connection test
         if "/ping" in request:
             self.reply(client, "pong")
             return None
 
+        # Send current sensor data
         if "/data" in request:
             self.reply(client, self.live_data)
             return None
 
+        # Check for movement commands
         if "move=F" in request:
             self.reply(client, "forward")
             return "F"
@@ -50,15 +64,22 @@ class RobotServer:
             self.reply(client, "right")
             return "R"
 
+        # Stop the robot if no valid movement command is received
         self.reply(client, "stop")
         return "S"
 
+    # Define function to send a response to the client
     def reply(self, client, message):
+
+        # Send HTTP response headers
         client.send("HTTP/1.1 200 OK\r\n")
         client.send("Content-Type: text/plain\r\n")
         client.send("Access-Control-Allow-Origin: *\r\n")
         client.send("Access-Control-Allow-Methods: GET, OPTIONS\r\n")
         client.send("Access-Control-Allow-Headers: *\r\n")
         client.send("Connection: close\r\n\r\n")
+
+        # Send the response message
         client.send(str(message))
         client.close()
+        
